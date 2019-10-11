952728c8dfed16c6c8e9ead825f11a7a8b2c43fa
Sadik <sadikurrahman41@gmail.com>
2019-10-11 23:31:17 +0600

    Update

---
+++ Add.java
@@ -0,0 +1,20 @@
+class Add
+{
+	
+ public static void main(String args[])	throws Exception
+ {
+	int i=5;
+    int j=4;
+    int result;
+
+    result= i + j;
+    System.out.println("The Sum is : "+result);	
+	
+	int x=10;
+	System.out.println(x);
+	 
+	 
+ }
+	
+	
+}
