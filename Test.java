4f2de4507dc14545fdfad820ceb49b7148499b8c
Sadik <sadikurrahman41@gmail.com>
2019-10-11 23:38:36 +0600

    Update file

--- Test.java
+++ Test.java
@@ -3,15 +3,18 @@ class Add
 	
  public static void main(String args[])	throws Exception
  {
-	int i=5;
+    int i=5;
     int j=4;
     int result;
 
     result= i + j;
     System.out.println("The Sum is : "+result);	
 	
-	int x=10;
-	System.out.println(x);
+    int x=10;
+    System.out.println(x);
+    String s="I am Sadikur Rahman\n I have one daughter\n She is name Maimuna Siddiqa";
+    System.out.println(s);
+
 	 
 	 
  }
